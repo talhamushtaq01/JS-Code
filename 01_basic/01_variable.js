8c9bc6f1f83120ce5c83600a1fde66f2cc7299d5
Muhammad Talha Mushtaq <147606096+talhamushtaq01@users.noreply.github.com>
2024-12-26 13:32:18 +0000

    container config done

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,10 @@
+const accountId = 1111;
+let accountEmail = "talha@gmail.com";
+var accountPassword = 1234;
+
+/*
+Perfer not use var 
+because of block scope and functional scope 
+*/
+accountEmail="aligmail.com";
+console.table([accountId,accountEmail,accountPassword]);
---
+++ 01_basic/02.js
@@ -0,0 +1,7 @@
+"Use new strick"; //all js code treate as new verison
+
+//alert(3+3);  // we are using node js, not browser
+
+console.log(3+3); // readabilty of code is so most imaportant
+
+console.log("Hlo Talha");
