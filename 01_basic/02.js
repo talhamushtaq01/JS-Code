eafa7be67e42a740b5f2d4da74f32d2b7df5acf9
Muhammad Talha Mushtaq <147606096+talhamushtaq01@users.noreply.github.com>
2024-12-26 13:56:13 +0000

    container config done

--- 01_basic/02.js
+++ 01_basic/02.js
@@ -5,3 +5,14 @@
 console.log(3+3); // readabilty of code is so most imaportant
 
 console.log("Hlo Talha");
+
+//link of website
+// https://tc39.es/ecma262/
+//  https://developer.mozilla.org/en-US/docs/Web/JavaScript
+
+
+// datatypes
+// number , string, boolen, BigInt,Symbol,undefined, null
+
+//how we check the type of variable
+console.log(typeof("talha"));
