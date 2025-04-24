f652316c06caf2c1b80ab4357f3cdcee90f02e57
Muhammad Talha Mushtaq <147606096+talhamushtaq01@users.noreply.github.com>
2025-04-24 14:47:59 +0000

    containwer

--- 01_basic/datatype-summary-interview.js
+++ 01_basic/datatype-summary-interview.js
@@ -8,6 +8,11 @@
 
 //7 types of primitive datatype
 // String, Number, Boolean, Null, Undefined. Symbol, Bigint
+
+// variable declare and space declare but value is not is declare is called undefined.
+
+// Symbol is used for declare unique value.
+
  
 //2  Non-Primitive Datatype   or reference type
 //Array, objects, Functions
